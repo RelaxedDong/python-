615c58b2d71c3ad2444d3ce04941c74d2891d0f6
donghao <1417766861@qq.com>
2018-08-04 19:28:36 +0800

    电影天堂爬虫功能完成

--- 网络请求/爬虫实例/电影天堂爬虫/dytt.py
+++ 网络请求/爬虫实例/电影天堂爬虫/dytt.py
@@ -3,33 +3,48 @@
 import requests
 from lxml import etree
 
-url = 'http://www.dytt8.net/html/gndy/dyzz/list_23_1.html'
+
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36',
 }
 
-response = requests.get(url,headers=headers)
-text = response.content.decode('gbk')
-html = etree.HTML(text)
-#详情页面获取
-# hrefs = html.xpath('//b/a/@href')
-# for href in hrefs:
-#     print('http://www.dytt8.net/'+href)
+
+def get_detaul_url():
+    movies = []
+    url = 'http://www.dytt8.net/html/gndy/dyzz/list_23_{}.html'
+    for index in range(1,2):
+        detail_url = url.format(index)
+        response = requests.get(detail_url, headers=headers)
+        text = response.content.decode('gbk')
+        html = etree.HTML(text)
+        hrefs = html.xpath('//b/a/@href')
+        for href in hrefs:
+            href = 'http://www.dytt8.net/' + href
+            movie = get_detail(href)
+            movies.append(movie)
 
 
-detail_url = 'http://www.dytt8.net/html/gndy/dyzz/20180731/57193.html'
-def get_detail():
+
+
+# detail_url = 'http://www.dytt8.net/html/gndy/dyzz/20180731/57193.html'
+#
+
+def get_detail(detail_url):
     de_response = requests.get(detail_url,headers=headers)
     de_text = de_response.content.decode('gbk')
     de_html = etree.HTML(de_text)
     movie ={}
-    xxx = de_html.xpath('//span[@style="FONT-SIZE: 12px"]/a/@href')
-    print(xxx)
     infos = de_html.xpath('//div[@id="Zoom"]//p[1]/text()')
-
-
+    download_url = de_html.xpath('//td//a[1]/text()')
+    poster =  de_html.xpath('//div[@id="Zoom"]//p[1]//img[1]/@src')
+    haibao = de_html.xpath('//div[@id="Zoom"]//p[1]//img[2]/@src')
+    title = de_html.xpath('//font[@color="#07519a"]//text()')[0]
+    print(title)
+    movie['title']=title
+    movie['poster']=poster
+    movie['haibao']=haibao
+    movie['download_url'] = download_url
     for index,info in enumerate(infos):
-
         if info.startswith("◎年　　代"):
             year = info.replace('◎年　　代',"").strip()
             movie['year'] = year
@@ -60,14 +75,7 @@ def get_detail():
         elif info.startswith("◎导　　演"):
             description = info.replace('◎导　　演', "").strip()
             movie['description'] = description
-
-
-
-
-    poster =  de_html.xpath('//div[@id="Zoom"]//p[1]//img[1]/@src')
-    haibao = de_html.xpath('//div[@id="Zoom"]//p[1]//img[2]/@src')
-    print(poster)
-    print(haibao)
+    return movie
 if __name__ == '__main__':
-    get_detail()
+    get_detaul_url()
 
